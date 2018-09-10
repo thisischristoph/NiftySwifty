9a45119802d36e04d3347f3460498961bc7a7689
Harrison, Chris (UK - London) <chrisharrison@deloitte.co.uk>
2018-09-10 14:47:29 +0100

    Initial Commit

---
+++ NiftySwifty/NiftySwifty.h
@@ -0,0 +1,19 @@
+//
+//  NiftySwifty.h
+//  NiftySwifty
+//
+//  Created by Harrison, Chris (UK - London) on 10/09/2018.
+//  Copyright © 2018 Harrison, Chris (UK - London). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NiftySwifty.
+FOUNDATION_EXPORT double NiftySwiftyVersionNumber;
+
+//! Project version string for NiftySwifty.
+FOUNDATION_EXPORT const unsigned char NiftySwiftyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NiftySwifty/PublicHeader.h>
+
+
